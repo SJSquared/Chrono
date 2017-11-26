21432b0a85d9417680e8591aaf903c332f6ae416
jimmybnguyen <jimmyn9@uw.edu>
2017-11-25 17:23:35 -0800

    add auth creation

--- Chrono/Chrono/ViewController.swift
+++ Chrono/Chrono/ViewController.swift
@@ -7,9 +7,32 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    @IBOutlet weak var actionButton: UIButton!
+    
+    @IBAction func action(_ sender: UIButton) {
+        if emailText.text != "" && passwordText.text != "" {
+            if segmentControl.selectedSegmentIndex == 0 { //sign-up
+                Auth.auth().createUser(withEmail: emailText.text!, password: passwordText.text!) { (user, error) in
+                    // ...
+                }
+                
+            } else { //login user
+                Auth.auth().signIn(withEmail: emailText.text!, password: passwordText.text!) { (user, error) in
+                    // ...
+                }
+            }
+        }
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
