1afbe6eb31226cb8ec677cafa54b63b3626302f5
jimmybnguyen <jimmyn9@uw.edu>
2017-11-25 22:34:19 -0800

    started onboarding screens

--- Chrono/Chrono/ViewController.swift
+++ Chrono/Chrono/ViewController.swift
@@ -15,23 +15,35 @@ class ViewController: UIViewController {
     @IBOutlet weak var passwordText: UITextField!
     @IBOutlet weak var segmentControl: UISegmentedControl!
     @IBOutlet weak var actionButton: UIButton!
+    @IBOutlet weak var signupButton: UIButton!
     
     @IBAction func action(_ sender: UIButton) {
         if emailText.text != "" && passwordText.text != "" {
             if segmentControl.selectedSegmentIndex == 0 { //sign-up
                 Auth.auth().createUser(withEmail: emailText.text!, password: passwordText.text!) { (user, error) in
-                    // ...
+                    if (user != nil) {
+                        self.performSegue(withIdentifier: "toOnboard", sender: self)
+                    }
                 }
                 
             } else { //login user
                 Auth.auth().signIn(withEmail: emailText.text!, password: passwordText.text!) { (user, error) in
-                    // ...
+                    if (user != nil) {
+                        self.performSegue(withIdentifier: "toOnboard", sender: self)
+                    }
                 }
             }
         }
         
     }
     
+    @IBAction func segmentControlAction(_ sender: Any) {
+        if segmentControl.selectedSegmentIndex == 1 {
+            signupButton.setTitle("Login", for: .normal)
+        } else {
+            signupButton.setTitle("Sign-up", for: .normal)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
