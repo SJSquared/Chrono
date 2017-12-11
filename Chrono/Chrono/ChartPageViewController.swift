cae354ea721b0c6d41dc19e79b75b91017d7db09
sarahfeldmann <sarahf95@gmail.com>
2017-12-11 01:11:29 -0800

    ver loose skeleton of two pages of charts

--- Chrono/Chrono/ChartPageViewController.swift
+++ Chrono/Chrono/ChartPageViewController.swift
@@ -17,7 +17,7 @@ var pageControl = UIPageControl()
 // MARK: UIPageViewControllerDataSource
 
 lazy var orderedViewControllers: [UIViewController] = {
-    return [self.newVc(viewController: "bar"),
+    return [self.newVc(viewController: "bar"), self.newVc(viewController: "bar2"),
             self.newVc(viewController: "line")]
 }()
 
@@ -48,7 +48,7 @@ func configurePageControl() {
     self.pageControl.currentPage = 0
     self.pageControl.tintColor = UIColor.black
     self.pageControl.pageIndicatorTintColor = UIColor.white
-    self.pageControl.currentPageIndicatorTintColor = UIColor.black
+    self.pageControl.currentPageIndicatorTintColor = UIColor.darkGray
     self.view.addSubview(pageControl)
 }
 
@@ -57,7 +57,7 @@ func newVc(viewController: String) -> UIViewController {
 }
 
 
-// MARK: Delegate methords
+// MARK: Delegate methods
 func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
     let pageContentViewController = pageViewController.viewControllers![0]
     self.pageControl.currentPage = orderedViewControllers.index(of: pageContentViewController)!
@@ -74,9 +74,7 @@ func pageViewController(_ pageViewController: UIPageViewController, viewControll
     // User is on the first view controller and swiped left to loop to
     // the last view controller.
     guard previousIndex >= 0 else {
-        return orderedViewControllers.last
-        // Uncommment the line below, remove the line above if you don't want the page control to loop.
-        // return nil
+         return nil
     }
     
     guard orderedViewControllers.count > previousIndex else {
@@ -97,9 +95,7 @@ func pageViewController(_ pageViewController: UIPageViewController, viewControll
     // User is on the last view controller and swiped right to loop to
     // the first view controller.
     guard orderedViewControllersCount != nextIndex else {
-        return orderedViewControllers.first
-        // Uncommment the line below, remove the line above if you don't want the page control to loop.
-        // return nil
+         return nil
     }
     
     guard orderedViewControllersCount > nextIndex else {
@@ -108,102 +104,5 @@ func pageViewController(_ pageViewController: UIPageViewController, viewControll
     
     return orderedViewControllers[nextIndex]
 }
-
-
-
-
-
-//class ChartPageViewController: UIPageViewController, UIPageViewControllerDelegate, UIPageViewControllerDataSource {
-//
-//    lazy var orderedViewControllers: [UIViewController] = {
-//        return [self.newVc(viewController: "line"),
-//                self.newVc(viewController: "bar")]
-//    }()
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        self.dataSource = self
-//        self.delegate = self
-//
-//        if let firstViewController = orderedViewControllers.first {
-//            setViewControllers([firstViewController],
-//                               direction: .forward,
-//                               animated: true,
-//                               completion: nil)
-//        }
-//        // Do any additional setup after loading the view.
-//    }
-//
-//    func newVc(viewController: String) -> UIViewController {
-//        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier:viewController)
-//    }
-//
-//
-////    // MARK: Delegate methords
-////    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
-////        let pageContentViewController = pageViewController.viewControllers![0]
-////    }
-////
-//    // MARK: Data source functions.
-//    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
-//        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
-//            return nil
-//        }
-//
-//        let previousIndex = viewControllerIndex - 1
-//
-//        // User is on the first view controller and swiped left to loop to
-//        // the last view controller.
-//        guard previousIndex >= 0 else {
-//            return orderedViewControllers.last
-//            // Uncommment the line below, remove the line above if you don't want the page control to loop.
-//            // return nil
-//        }
-//
-//        guard orderedViewControllers.count > previousIndex else {
-//            return nil
-//        }
-//
-//        return orderedViewControllers[previousIndex]
-//    }
-//
-//    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
-//        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
-//            return nil
-//        }
-//
-//        let nextIndex = viewControllerIndex + 1
-//        let orderedViewControllersCount = orderedViewControllers.count
-//
-//        // User is on the last view controller and swiped right to loop to
-//        // the first view controller.
-//        guard orderedViewControllersCount != nextIndex else {
-//            return orderedViewControllers.first
-//            // Uncommment the line below, remove the line above if you don't want the page control to loop.
-//            // return nil
-//        }
-//
-//        guard orderedViewControllersCount > nextIndex else {
-//            return nil
-//        }
-//
-//        return orderedViewControllers[nextIndex]
-//    }
-//
-//    override func didReceiveMemoryWarning() {
-//        super.didReceiveMemoryWarning()
-//        // Dispose of any resources that can be recreated.
-//    }
-//
-//
-//    /*
-//    // MARK: - Navigation
-//
-//    // In a storyboard-based application, you will often want to do a little preparation before navigation
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        // Get the new view controller using segue.destinationViewController.
-//        // Pass the selected object to the new view controller.
-//    }
-//    */
 
 }
--- Chrono/Chrono/ChartViewController.swift
+++ Chrono/Chrono/ChartViewController.swift
@@ -12,9 +12,22 @@ import Charts
 class ChartViewController: UIViewController {
  
     
-    @IBOutlet weak var barView: BarChartView!
-//    @IBOutlet weak var lineView: LineChartView!
+    @IBOutlet weak var barView2: BarChartView!
+    @IBOutlet weak var pieChart: PieChartView!
     
+//    @IBOutlet var barView2: BarChartView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        updateChartWithData()
+//        pieChartUpdate()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
     
     func updateChartWithData(){
         var dataEntries: [BarChartDataEntry] = []
@@ -24,31 +37,39 @@ class ChartViewController: UIViewController {
         }
         let chartDataSet = BarChartDataSet(values: dataEntries, label: "Hours logged")
         let chartData = BarChartData(dataSet: chartDataSet)
-        barView.data = chartData
+        barView2.data = chartData
     }
     
+    func pieChartUpdate () {
+        
+        // Basic set up of plan chart
+        
+        let entry1 = PieChartDataEntry(value: Double(4), label: "#1")
+        let entry2 = PieChartDataEntry(value: Double(3), label: "#2")
+        let entry3 = PieChartDataEntry(value: Double(6), label: "#3")
+        let dataSet = PieChartDataSet(values: [entry1, entry2, entry3], label: "Widget Types")
+        let data = PieChartData(dataSet: dataSet)
+        pieChart.data = data
+        pieChart.chartDescription?.text = "Share of Widgets by Type"
+        
+        // Color
+        dataSet.colors = ChartColorTemplates.joyful()
+        //dataSet.valueColors = [UIColor.black]
+        pieChart.backgroundColor = UIColor.black
+        pieChart.holeColor = UIColor.clear
+        pieChart.chartDescription?.textColor = UIColor.white
+        pieChart.legend.textColor = UIColor.white
+        
+        // Text
+        pieChart.legend.font = UIFont(name: "Futura", size: 10)!
+        pieChart.chartDescription?.font = UIFont(name: "Futura", size: 12)!
+        pieChart.chartDescription?.xOffset = pieChart.frame.width
+        pieChart.chartDescription?.yOffset = pieChart.frame.height * (2/3)
+        pieChart.chartDescription?.textAlign = NSTextAlignment.left
+        
+        // Refresh chart with new data
+        pieChart.notifyDataSetChanged()
+    }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        updateChartWithData()
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
