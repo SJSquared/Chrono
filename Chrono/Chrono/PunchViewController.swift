f559215ef5711c24dce5435ea67ea966e0227a54
Jingyu Yang <yangj9@uw.edu>
2017-12-11 22:11:51 -0800

    adding interval when you clock out

--- Chrono/Chrono/PunchViewController.swift
+++ Chrono/Chrono/PunchViewController.swift
@@ -10,29 +10,25 @@ import UIKit
 import Firebase
 import FirebaseDatabase
 
-class PunchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
-    }
-    
+class PunchViewController: UIViewController {
+   
 
     @IBOutlet weak var currTime: UILabel!
     @IBOutlet weak var clockIn: UIButton!
     @IBOutlet weak var mealOut: UIButton!
     @IBOutlet weak var mealIn: UIButton!
     @IBOutlet weak var clockOut: UIButton!
-    @IBOutlet weak var table: UITableView!
     var timer = Timer()
     var uid: String!
+    var clockInTime : Date!
     var ref: DatabaseReference!
     var company: String!
     
+   
+    
     @IBAction func clickClockIn(_ sender: Any) {
         let currTime = Date()
+        self.clockInTime = currTime
         writeData("cIn", currTime)
     }
     
@@ -49,6 +45,7 @@ class PunchViewController: UIViewController, UITableViewDelegate, UITableViewDat
     @IBAction func clickClockOut(_ sender: Any) {
         let currTime = Date()
         writeData("cOut", currTime)
+        
     }
     
     func writeData(_ type: String, _ time: Date) {
@@ -58,15 +55,16 @@ class PunchViewController: UIViewController, UITableViewDelegate, UITableViewDat
         dateFormatter.locale = Locale(identifier: "en_US")
         print(dateFormatter.string(from: time))
         let date = dateFormatter.string(from: time)
-        self.ref.child("companies").child(self.company).child(self.uid).child(date + "/" + type).setValue(DateFormatter.localizedString(from: time,
-                                                                                            dateStyle: .medium,
-                                                                                                            timeStyle: .medium))
-
+        self.ref.child("companies").child(self.company).child(self.uid).child(date + "/" + type).setValue(DateFormatter.localizedString(from: time, dateStyle: .medium, timeStyle: .medium))
+        if (type == "cOut") {
+            self.ref.child("companies").child(self.company).child(self.uid).child(date + "/interval").setValue(time.timeIntervalSince(self.clockInTime))
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         ref = Database.database().reference()
+        clockInTime = Date()
         timer = Timer.scheduledTimer(timeInterval: 1.0,
             target: self,
             selector: #selector(tick),
