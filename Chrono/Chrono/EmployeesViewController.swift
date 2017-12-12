4110241437d0228e802204fa3cc564ea8fc912cc
jimmybnguyen <jimmyn9@uw.edu>
2017-12-11 19:31:04 -0800

    merge with jenny branch

--- Chrono/Chrono/EmployeesViewController.swift
+++ Chrono/Chrono/EmployeesViewController.swift
@@ -6,4 +6,31 @@
 //  Copyright © 2017 SJSquared. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import Firebase
+import FirebaseDatabase
+
+class EmployeesViewController: UIViewController {
+    var ref: DatabaseReference!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        ref = Database.database().reference()
+        
+        let handle = Auth.auth().addStateDidChangeListener { (auth, user) in
+            var currentCompany : String = ""
+            self.ref.child("users").child(user!.uid).observeSingleEvent(of: .value, with: { (snapshot) in
+                let userValue = snapshot.value as? NSDictionary
+                currentCompany = userValue?["currentCompany"] as! String
+                print(userValue)
+                print(currentCompany)
+            })
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
