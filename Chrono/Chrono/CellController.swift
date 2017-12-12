2654a6e75a03a67554a006f688d6b4d670f16275
jimmybnguyen <jimmyn9@uw.edu>
2017-12-11 20:24:56 -0800

    add cellcontroller

---
+++ Chrono/Chrono/CellController.swift
@@ -0,0 +1,19 @@
+//
+//  CellController.swift
+//  Chrono
+//
+//  Created by iGuest on 12/11/17.
+//  Copyright © 2017 SJSquared. All rights reserved.
+//
+import UIKit
+
+class CellController: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
