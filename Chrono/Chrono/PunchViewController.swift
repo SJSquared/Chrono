30d257c2281ce188a13f134bf766c205bd446800
Jingyu Yang <yangj9@uw.edu>
2017-12-12 16:03:06 -0800

    added today history

--- Chrono/Chrono/PunchViewController.swift
+++ Chrono/Chrono/PunchViewController.swift
@@ -13,6 +13,10 @@ import FirebaseDatabase
 class PunchViewController: UIViewController {
    
 
+    @IBOutlet weak var inTime: UILabel!
+    @IBOutlet weak var mOutTime: UILabel!
+    @IBOutlet weak var mInTime: UILabel!
+    @IBOutlet weak var outTime: UILabel!
     @IBOutlet weak var currTime: UILabel!
     @IBOutlet weak var clockIn: UIButton!
     @IBOutlet weak var mealOut: UIButton!
@@ -23,7 +27,7 @@ class PunchViewController: UIViewController {
     var clockInTime : Date!
     var ref: DatabaseReference!
     var company: String!
-    
+    var date : String!
    
     
     @IBAction func clickClockIn(_ sender: Any) {
@@ -54,8 +58,8 @@ class PunchViewController: UIViewController {
         dateFormatter.timeStyle = .none
         dateFormatter.locale = Locale(identifier: "en_US")
         print(dateFormatter.string(from: time))
-        let date = dateFormatter.string(from: time)
-        self.ref.child("companies").child(self.company).child(self.uid).child(date + "/" + type).setValue(DateFormatter.localizedString(from: time, dateStyle: .medium, timeStyle: .medium))
+        date = dateFormatter.string(from: time)
+        self.ref.child("companies").child(self.company).child(self.uid).child(self.date + "/" + type).setValue(DateFormatter.localizedString(from: time, dateStyle: .none, timeStyle: .medium))
         if (type == "cOut") {
             self.ref.child("companies").child(self.company).child(self.uid).child(date + "/interval").setValue(time.timeIntervalSince(self.clockInTime))
         }
@@ -63,20 +67,42 @@ class PunchViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        ref = Database.database().reference()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .none
+        dateFormatter.locale = Locale(identifier: "en_US")
+        self.date = dateFormatter.string(from: Date())
+        
+        self.ref = Database.database().reference()
         clockInTime = Date()
         timer = Timer.scheduledTimer(timeInterval: 1.0,
             target: self,
             selector: #selector(tick),
             userInfo: nil,
             repeats: true)
-        uid = Auth.auth().currentUser?.uid
-        ref.child("users").child(uid!).observeSingleEvent(of: .value, with: { (snapshot) in
-            let value = snapshot.value as? NSDictionary
-            self.company = value?["currentCompany"] as? String ?? ""
-            }) { (error) in
-            print(error.localizedDescription)
-        }
+        self.uid = Auth.auth().currentUser?.uid
+        let usersReference = self.ref.child("users").child(self.uid)
+        let r2 = self.ref.child("companies")
+        
+        var refHandle = self.ref.observe(DataEventType.value, with: { (snapshot) in
+            
+            let allValue = snapshot.value as? [String : NSDictionary] ?? [:]
+            let allComp = allValue["companies"] as? [String : NSDictionary] ?? [:]
+            let allUsers = allValue["users"] as? [String : NSDictionary] ?? [:]
+            let currUser = allUsers[self.uid] as? [String : String] ?? [:]
+            print("HIIIII " + currUser["currentCompany"]!)
+            self.company = currUser["currentCompany"]
+
+            let thisComp = allComp[self.company] as? [String : NSDictionary] ?? [:]
+            let thisUser = thisComp[self.uid] as? [String : Any] ?? [:]
+            let today = thisUser[self.date] as? [String : Any] ?? [:]
+            
+            self.inTime.text = "Clocked In: " + (today["cIn"] != nil ? (today["cIn"] as! String): "")
+            self.mOutTime.text = "Meal Out: " + (today["mOut"] != nil ? (today["mOut"] as! String): "")
+            self.mInTime.text = "Meal In: " + (today["mIn"] != nil ? (today["mIn"] as! String): "")
+            self.outTime.text = "Clocked Out: " + (today["cOut"] != nil ? (today["cOut"] as! String): "")
+
+        })
     }
 
     @objc func tick() {
